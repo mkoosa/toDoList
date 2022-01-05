9522003f35c18b1fb30b86c22aa01d646ea6589e
Marcin Kosecki <marcin.kosecki@int.pl>
2022-01-05 20:02:52 +0100

    create inputTxt , clearInputTxt in class InterfaceUser

--- interfaceUser.esm.js
+++ interfaceUser.esm.js
@@ -1,11 +1,13 @@
 import { NoteElement } from "./noteElement.esm.js";
 
-const ADD_BTN = "addBtn";
-const NOTE = "note";
+const ADD_BTN_ID = "addBtn";
+const NOTE_ID = "note";
+const NOTE_TXT_ID = "noteTxt";
 
 class InterfaceUser {
   createDomElements() {
-    this.addBtn = this.#bindToElements(ADD_BTN);
+    this.addBtn = this.#bindToElements(ADD_BTN_ID);
+    this.input = this.#bindToElements(NOTE_TXT_ID);
     this.events();
   }
 
@@ -20,16 +22,28 @@ class InterfaceUser {
 
   creatNote() {
     this.addBtn.addEventListener("click", this.note);
+    this.addBtn.addEventListener("click", this.inputTxt);
+    this.addBtn.addEventListener("click", this.clearInputTxt);
   }
 
   note = () => {
-    this.noteElement = new NoteElement(this.#bindToElements(NOTE));
+    this.noteElement = new NoteElement(this.#bindToElements(NOTE_ID));
 
     this.noteElement.noteElements();
+  };
+
+  inputTxt = () => {
+    const textInput = this.input.value;
+    return textInput;
+  };
+
+  clearInputTxt = () => {
+    this.input.value = "";
   };
 }
 
 window.onload = function () {
-  const interfaceUser = new InterfaceUser();
   interfaceUser.createDomElements();
 };
+
+export const interfaceUser = new InterfaceUser();
