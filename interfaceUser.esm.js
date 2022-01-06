dce9b57361ec3496fe03b424ed49949e7986f961
Marcin Kosecki <marcin.kosecki@int.pl>
2022-01-06 13:52:42 +0100

    add array allEvents

--- interfaceUser.esm.js
+++ interfaceUser.esm.js
@@ -1,13 +1,19 @@
-import { NoteElement } from "./noteElement.esm.js";
+import { Note } from "./noteElement.esm.js";
 
 const ADD_BTN_ID = "addBtn";
 const NOTE_ID = "note";
 const NOTE_TXT_ID = "noteTxt";
+const CONTAINER_ID = "container";
 
 class InterfaceUser {
+  constructor() {
+    this.allEvents = [];
+    // console.log('this.allEvents :', this.allEvents);
+  }
   createDomElements() {
     this.addBtn = this.#bindToElements(ADD_BTN_ID);
     this.input = this.#bindToElements(NOTE_TXT_ID);
+    this.container = this.#bindToElements(CONTAINER_ID);
     this.events();
   }
 
@@ -22,18 +28,27 @@ class InterfaceUser {
 
   creatNote() {
     this.addBtn.addEventListener("click", this.note);
-    this.addBtn.addEventListener("click", this.inputTxt);
+    // this.addBtn.addEventListener("click", this.inputTxt);
     this.addBtn.addEventListener("click", this.clearInputTxt);
   }
 
   note = () => {
-    this.noteElement = new NoteElement(this.#bindToElements(NOTE_ID));
+    this.noteElement = new Note(this.#bindToElements(NOTE_ID));
+    console.log("this.noteElement :", this.noteElement);
+    const element = this.noteElement.noteElements();
 
-    this.noteElement.noteElements();
+    this.allEvents.push(element);
+    console.log("element :", element.noteContainer);
+
+    console.log("this.allEvents :", this.allEvents);
+    element.label.textContent = this.inputTxt();
+
+    element.editBtn.addEventListener("click", this.noteElement.editTextNote);
   };
 
   inputTxt = () => {
     const textInput = this.input.value;
+    console.log("textInput :", textInput);
     return textInput;
   };
 
